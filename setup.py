6f57703dd00c584a4de52e2c5f31ea27e9ffac42
Alon Horev <alonho@gmail.com>
2013-01-27 22:34:31 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mql',
+      version='0.0.1',
+      description='A python expression to MongoDB query translator',
+      author='Alon Horev',
+      author_email='alon@horev.net',
+      url='https://github.com/alonho/mql',
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Operating System :: POSIX :: Linux",
+                     "Operating System :: MacOS :: MacOS X"],
+      license='BSD',
+      py_modules=['mql'])
